7d0c6bfccfecf8b5a987adf62e7cd362af61602b
William Merussi <wmerussi@gmail.com>
2019-08-07 14:57:08 -0300

    added item model

---
+++ src/app/models/item.model.ts
@@ -0,0 +1,26 @@
+import { Model } from './model';
+
+export enum ItemType {
+  BUY = 'buy',
+  SELL = 'sell',
+}
+
+export class Item extends Model {
+  public name: string;
+  public type: ItemType;
+  public value: number;
+
+  constructor(obj?: any) {
+    super(obj);
+  }
+
+  public set valueToNumber(value: string) {
+    this.value = Number(value);
+  }
+
+  public clear() {
+    delete this.name;
+    delete this.type;
+    delete this.value;
+  }
+}
