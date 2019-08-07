69241fd150f67bf6557a93c1a8c1ff3b4595353b
William Merussi <wmerussi@gmail.com>
2019-08-07 13:27:21 -0300

    added home page layout and some logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { BreakpointService } from './services/breakpoint.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'transaction-manager';
+export class AppComponent implements OnInit, OnDestroy {
+  public hideBox: boolean;
+
+  private breakpoint: BreakpointService = new BreakpointService('md');
+
+  ngOnInit() {
+    this.breakpoint.get().subscribe(md => this.hideBox = !md);
+  }
+
+  ngOnDestroy() {
+    this.breakpoint.removeListener();
+  }
+
+  public onTypeChange (type) {
+    console.log(type);
+  }
+
+  public onItemNameChange (name) {
+    console.log(name);
+  }
+
+  public onItemValueChange (value) {
+    console.log(value);
+  }
 }
